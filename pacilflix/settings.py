b6795480d19f3b4c52b6ad3cd1b01c0278971473
mma <111668501+mmalkindi@users.noreply.github.com>
2024-05-17 17:06:31 +0700

    Fix static files set up

--- pacilflix/settings.py
+++ pacilflix/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os, environ
+import os, environ, mimetypes
 from pathlib import Path
 
 # Initialise environment variables
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%6bsjc-((c8=cc(9l-@3v&k-pzl_ix(knqus0%%%_yriew72lf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG_ENABLED', True)
 
 ALLOWED_HOSTS = ["*"]
 
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
     'django_countries',
     'authentication',
     'content',
@@ -136,9 +135,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT =  BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".js", True)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
