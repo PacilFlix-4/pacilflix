5667d46c5f835b5fb3e4e9cffb6c404c35fea51f
Michelle Elizabeth Amanda Hutasoit <michelleelizabethamandahutasoit@Michelles-MacBook-Pro.local>
2024-05-17 23:49:44 +0700

    Integrate with backend

--- download/views.py
+++ download/views.py
@@ -1,64 +1,83 @@
-from itertools import product
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.core import serializers
+import datetime
+from django.db import connection
+from django.shortcuts import redirect, render
+from django.http import HttpResponseNotAllowed, JsonResponse
+from authentication.views import get_pengguna
 
-downloaded_shows = [['Parasite', 'rainbowbridge', '2024-01-01 23:57:28'],
-    ['A Man Called Otto', 'rainbowbridge', '2024-01-02 14:13:05'],
-    ['Pitch Perfect', 'rainbowbridge', '2024-01-03 19:13:02'],
-    ['La La Land', 'rainbowbridge', '2024-01-04 11:10:45'],
-    ['The Greatest Showman', 'rainbowbridge', '2024-01-05 20:01:51'],
-    ['Jurassic World', 'kentangnyamekdi', '2024-02-01 12:54:22'],
-    ['The Hunger Games: The Ballad of Songbirds & Snakes', 'kentangnyamekdi', '2024-03-01 10:40:48'],
-    ['The Maze Runner', 'kentangnyamekdi', '2024-01-04 11:10:09'],
-    ['Godzilla King of The Monsters', 'kentangnyamekdi', '2024-01-05 21:48:51'],
-    ['Dune', 'kentangnyamekdi', '2024-01-06 12:30:58'],
-    ['The Silent Sea', 'kentangnyamekdi', '2024-01-07 12:54:27'],
-    ['Bridgerton', 'kentangnyamekdi', '2024-01-08 11:40:57'],
-    ['WandaVision', 'cottonthedog', '2024-01-03 10:41:53'],
-    ['Alice in Borderland', 'kodokronggeng', '2024-01-04 17:04:53'],
-    ['Wednesdays', 'kodokronggeng', '2024-01-05 18:45:56'],
-    ['Parasite', 'kodokronggeng', '2024-01-06 16:29:33'],
-    ['A Man Called Otto', 'berkelapkelip', '2024-01-05 18:51:48'],
-    ['Pitch Perfect', 'berkelapkelip', '2024-01-06 14:42:36'],
-    ['La La Land', 'berkelapkelip', '2024-01-07 23:55:12'],
-    ['The Greatest Showman', 'berkelapkelip', '2024-01-08 15:47:01'],
-    ['Jurassic World', 'parahsihtamol', '2024-01-05 21:15:32'],
-    ['The Hunger Games: The Ballad of Songbirds & Snakes', 'parahsihtamol', '2024-01-06 20:59:34'],
-    ['The Maze Runner', 'parahsihtamol', '2024-01-07 14:36:09'],
-    ['Godzilla King of The Monsters', 'aizadiguno', '2024-01-07 13:18:18'],
-    ['Dune', 'aizadiguno', '2024-01-08 19:48:30'],
-    ['The Silent Sea', 'aizadiguno', '2024-01-09 16:02:19'],
-    ['Bridgerton', 'aizadiguno', '2024-01-10 11:27:20'],
-    ['WandaVision', 'kudaponi11', '2024-01-08 21:48:37'],
-    ['Alice in Borderland', 'kudaponi11', '2024-01-09 10:14:00'],
-    ['Wednesdays', 'kudaponi11', '2024-01-10 22:27:53']]
+def get_unduhan(username):
+    query = f"""
+        SELECT t.judul, t.id, tu.timestamp
+        FROM TAYANGAN T
+        JOIN TAYANGAN_TERUNDUH tu
+        ON t.id = tu.id_tayangan
+        WHERE tu.username = '{username}';
+    """
 
-def show_main(request):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    unduhan = cursor.fetchall()
+    cursor.close()
+
+    return unduhan
+
+def show_download(request):
+    username = get_pengguna(request)
+    daftar_favorit = get_unduhan(username)
+    
     context = {
-        'downloaded_shows': downloaded_shows,
+        'pengguna': username,
+        'daftar_unduhan': daftar_favorit
     }
 
     return render(request, "download.html", context)
 
-def show_xml(request):
-    data = downloaded_shows
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+def add_unduhan(request, id_tayangan):
+    username = get_pengguna(request)
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-def show_xml_by_id(request, id):
-    data = downloaded_shows
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    if request.method == 'POST':
+        query_update = f"""
+            INSERT INTO TAYANGAN_TERUNDUH (id_tayangan, username, timestamp)
+            VALUES ({id_tayangan}, {username}, {timestamp})
+        """
 
-def show_json(request):
-    data = downloaded_shows
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+        cursor = connection.cursor()
+        cursor.execute(query_update)
+        row_added = cursor.rowcount
+        cursor.close()
 
-def show_json_by_id(request, id):
-    data = downloaded_shows
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+        if row_added > 0:
+            return redirect('download:show_download')
+        else:
+            return redirect('download:show_download')
+        
+    else:
+        if request.method == 'GET':
+            return HttpResponseNotAllowed(['POST'], 'This action can only be performed with POST.')
+        else:
+            return HttpResponseNotAllowed(['POST'])
 
-def add_downloaded_show(request, id):
-    data = "" # Belum implement
+def delete_unduhan(request, id_tayangan, timestamp):
+    username = get_pengguna(request)
 
-def delete_downloaded_show(request, id):
-    data = "" # Belum implement
+    if request.method == 'POST':
+        query_update = f"""
+            DELETE FROM TAYANGAN_TERUNDUH
+            WHERE id_tayangan = '{id_tayangan}' AND timestamp = '{timestamp}' AND username = '{username}';
+        """
+
+        cursor = connection.cursor()
+        cursor.execute(query_update)
+        row_deleted = cursor.rowcount
+        cursor.close()
+
+        if row_deleted > 0:
+            return redirect('download:show_download')
+        else:
+            return redirect('download:show_download')
+        
+    else:
+        if request.method == 'GET':
+            return HttpResponseNotAllowed(['POST'], 'This action can only be performed with POST.')
+        else:
+            return HttpResponseNotAllowed(['POST'])
