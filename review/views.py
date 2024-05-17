8b79cc39e8e88b16cd0fdb677f646d828c6b0795
Fec16 <jasonkentwinata@gmail.com>
2024-05-17 22:29:17 +0700

    Update Film

--- review/views.py
+++ review/views.py
@@ -1,10 +1,39 @@
-from django.shortcuts import render
+import datetime
+from django.db import connection
+from django.shortcuts import redirect, render
 
-def show_main(request):
+# Create your views here.
+def ulasan(request, id_tayangan):
+    ulasans = []
+    with connection.cursor() as cursor:
+        cursor.execute(
+            f"SELECT * FROM ULASAN WHERE id_tayangan = '{id_tayangan}'")
+        ulasans = cursor.fetchall()
+
+        cursor.execute(
+            f"SELECT id, judul FROM TAYANGAN WHERE id = '{id_tayangan}'")
+        judul = cursor.fetchone()
+
     context = {
-        'name': 'Jason',
+        'judul' : judul,
+        'ulasans': ulasans,
     }
-    return render(request, "main_review.html", context)
+    response = render(request, 'ulasan.html', context)
+    return response
 
-def show_ulasan(request):
-    return render(request, "ulasan.html")
+def submit_ulasan(request, id_tayangan):
+    if request.method == 'POST':
+        username = request.COOKIES.get('username')
+        rating = request.POST.get('rating')
+        deskripsi = request.POST.get('deskripsi')
+        timestamp = datetime.datetime.now()
+
+        query = """
+        INSERT INTO ULASAN (id_tayangan, username, timestamp, rating, deskripsi)
+        VALUES (%s, %s, %s, %s, %s)
+        """
+        
+        with connection.cursor() as cursor:
+            cursor.execute(query, [id_tayangan, username, timestamp, rating, deskripsi])
+
+    return render(request, 'ulasan.html')
